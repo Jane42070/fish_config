1b5648a40d752dee23644514027d876216215ace
Jane42070 <jql1377219787@gmail.com>
2020-12-31 14:45:07 +0800

    sy "cowsay"; n "neofetch"

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 set mes tty | awk '{print $1}'
-if test "$mes" = "tty"
-	fbterm >/dev/null 2>&1
-end
+# if test "$mes" = "tty"
+#     fbterm >/dev/null 2>&1
+# end
 set fish_greeting ""
 set EDITOR nvim
 # UTF-8
@@ -12,7 +12,7 @@ source /usr/share/autojump/autojump.fish
 fish_vi_key_bindings
 # alias
 set PATH /home/pi/.cargo/bin/ $PATH
-set PATH /usr/local/llvm8/bin/ $PATH
+set PATH /usr/local/llvm11/bin/ $PATH
 # go procedure
 set PATH /home/pi/go/bin/ $PATH
 set PATH /usr/local/go/bin/ $PATH
@@ -25,8 +25,10 @@ alias pip "pip3"
 # set PATH /usr/local/node/bin/ $PATH
 alias ra "ranger"
 alias md "mkdir"
-alias se "du -a ~/.config/* ~/github/* |awk '{print \$2}' | fzf --preview 'bat --color=always --theme=gruvbox --decorations=never {}' --border | xargs -r nvim"
-alias vf "fzf --preview 'bat --color=always --theme=gruvbox --decorations=never {}' --border | xargs -r -I % nvim %"
+alias cy "cowsay"
+alias n  "neofetch"
+alias se "du -a ~/.config/* ~/github/* |awk '{print \$2}' | fzf --preview 'bat --color=always --theme=gruvbox {}' --border | xargs -r nvim"
+alias vf "fzf --preview 'bat --color=always --theme=gruvbox {}' --border | xargs -r -I % nvim %"
 alias apt "sudo apt"
 alias ep "export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890"
 # Set environment path
